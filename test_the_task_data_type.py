dbf595be7d324f5d20ff1e839e16d4cf9c7d45dd
Nischal Lal Shrestha <aakrist666@gmail.com>
2018-06-17 18:01:45 +0545

    Test for simple TaskManager

--- test_the_task_data_type.py
+++ test_the_task_data_type.py
@@ -1,10 +1,40 @@
 from collections import namedtuple
 
-Task = namedtuple('Task', ['summary' 'owner' 'done','id'])
-Task.__new__.__defaults__=(None,None,False,None)
+Task = namedtuple('Task', ['summary','owner','done','id'])
+Task.__new__.__defaults__ = (None, None, False, None)
 
 
 def test_defaults():
+    """Use of no parameters should invoke defaults"""
     t1 = Task()
     t2 = Task(None,None,False,None)
     assert t1 == t2
+
+def test_member_access():
+    """Check the field functionality of namedtuple"""
+    t = Task('Buy a milk', 'Nischal')
+    assert t.summary == 'Buy a milk'
+    assert t.owner == 'Nischal'
+    assert(t.done, t.id) == (False,None)
+
+
+def test_asdict():
+    """_asdit() should return a dictionary"""
+    t = Task("Call John", "Nischal", "Yes", 5)
+    t = t._asdict()
+    expected = {
+        'summary':'Call John',
+        'owner':'Nischal',
+        'done': 'Yes',
+        'id':5
+    }
+
+    assert t == expected
+
+
+def test_replace():
+    """replace"""
+    t_before = Task("Finish Book", 'Nischal')
+    t_after = t_before._replace(done='Yes',id=5)
+    t_expected = Task('Finish Book', 'Nischal', 'Yes', 5)
+    assert t_after == t_expected
